2d59a3b6a18240eb5db9467846743313edf4db7d
jaypalande1425 <jaypalande1425@gmail.com>
2025-02-06 12:28:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function firstWord(s) {
-  // your code here
+    let result = '';
+    
+    for (let i = 0; i < s.length; i++) {
+        // If we encounter a space, break the loop
+        if (s[i] === ' ') {
+            break;
+        }
+        // Otherwise, add the character to the result string
+        result += s[i];
+    }
+    
+    return result;
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
